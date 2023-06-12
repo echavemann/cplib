80579c9f8d46f33e8e65e81b005e1180225645b6
Ethan Havemann <94627102+echavemann@users.noreply.github.com>
2023-06-12 14:27:35 -0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <bits/stdc++.h>
 #define ll long long
-#define pb push_back
+#define PB push_back
 #define F first
 #define S second
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+#define MP make_pair;
 #define vt vector
+#define REP(i, a, b) for (int i = a; i < b; i++)
 using namespace std;
 
 void solve()
